a8bb14d31363e0ee62d42d71e79966614deaad80
Len Alford <lalford@eose.com>
2026-09-20 18:26:53 -0400

    Add files via upload
    -changed values to hex
    -added requirements.txt for users
    -added docopt flags for arguments, for future testing use

--- voltagetest.py
+++ voltagetest.py
@@ -1,8 +1,12 @@
+'''
+Usage: voltagetest.py <ilccip> (-O|-U)
+
+'''
 import requests
+from docopt import docopt
 '''test 1. voltage test.'''
 
-def voltagetest(link):
-    #payload = 
+def overvoltssu(link):
     thresholds = f'{link}api/thresholds'
     scbreadings = f'{link}api/scb/readings'
     ssureadings = f'{link}api/ssu/readings'
@@ -10,6 +14,8 @@ def voltagetest(link):
     t = requests.get(thresholds)
     u = requests.get(scbreadings)
     v = requests.get(ssureadings)
+    print(t.json())
+    print('tracer')
     headers = {'Content-Type': 'application/json'}
     #headers equivalent to -H curl command
     data = {'voltage': 9000}
@@ -22,9 +28,35 @@ def voltagetest(link):
     print(v.json())
     A = v.json()
     value = A['ofaults']
-    if value == 32:
+    return False
+    if value == 0x20:
+        return True
+    #response = requests.post(reset)
+    print(response)
+def undervoltssu(link):
+    thresholds = f'{link}api/thresholds'
+    scbreadings = f'{link}api/scb/readings'
+    ssureadings = f'{link}api/ssu/readings'
+    status = f'{link}api/status'
+    reset = f'{link}/api/reset'
+    v = requests.get(ssureadings)
+    headers = {'Content-Type': 'application/json'}
+    data = {'voltage': 1}
+    print(v.json())
+    response = requests.post(ssureadings, headers=headers, json=data)
+    print(v.json())
+    A = v.json()
+    value = A['ofaults']
+    print(value)
+    return False
+    if value == 0x10:
         return True
 if __name__ == "__main__":
-    ilccip = input()
-    if voltagetest('http://100.68.200.44:8999/'):
-        print("overvolt works!")
+    arguments = docopt(__doc__)
+    print(arguments)
+    if arguments['-O']:
+        overvoltssu(arguments['<ilccip>']) #http://100.68.200.44:8999
+        print("overvoltssu works!")
+    if arguments['-U']:
+        undervoltssu(arguments['<ilccip>'])
+        print("undervoltssu works!")
